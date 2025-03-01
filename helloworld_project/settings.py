76d5571760929de2cc49d67958f07fb7f009a2a0
JT7549 <3567286053@qq.com>
2025-03-02 00:06:09 +0800

    Initial commit

--- helloworld_project/settings.py
+++ helloworld_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-o=hu6i1q3v=pb#26-rnl7*n5s%=xq!5xliq^-=da11b3fzdvhv"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ["JT7549.github.io"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
